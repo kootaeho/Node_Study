045fb7bbcacbe4c314a1d7c684b42eef0d1e73b5
kootaeho <0307rnxogh@daum.net>
2024-02-08 23:40:36 +0900

    res.sendFile

--- _19_web_server/main.js
+++ _19_web_server/main.js
@@ -10,10 +10,12 @@ app.listen(3000, ()=>{
 //처리해주는 루틴 추가
 app.get('/', (req,res)=>{
     console.log("루트에 요청이 들어옴")
-    res.send("루트에 대한 요청")
+    //res.send("루트에 대한 요청")
+    res.sendFile(__dirname + '/pages/index.html')
 })
 
 app.get('/about', (req,res)=>{
     console.log("/about에 요청이 들어옴")
-    res.send("/about에 대한 요청")
+    //res.send("/about에 대한 요청")
+    res.sendFile(__dirname + '/pages/about.html')
 })
