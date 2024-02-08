4b2187576b37b0b33263e3cc2b43ae0e6f24d942
kootaeho <0307rnxogh@daum.net>
2024-02-08 23:30:35 +0900

    Node.get()

--- _19_web_server/main.js
+++ _19_web_server/main.js
@@ -3,5 +3,17 @@ const app = express()
 
 
 app.listen(3000, ()=>{
-    console.log("누군가가 접속함.")
+    console.log("gg 누군가가 접속함.")
+})
+
+
+//처리해주는 루틴 추가
+app.get('/', (req,res)=>{
+    console.log("루트에 요청이 들어옴")
+    res.send("루트에 대한 요청")
+})
+
+app.get('/about', (req,res)=>{
+    console.log("/about에 요청이 들어옴")
+    res.send("/about에 대한 요청")
 })
