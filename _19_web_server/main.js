997b17d916f6eb5798c45d43cb7180d16acf1a3b
kootaeho <0307rnxogh@daum.net>
2024-07-29 15:43:51 +0900

    dbconnection
    데베 연결함

--- _19_web_server/main.js
+++ _19_web_server/main.js
@@ -2,21 +2,21 @@ const express = require('express') //npm install express --save
 const app = express()
 
 
-app.use('/pages', express.static(__dirname+'/pages')) //pages 경로로 들어오는 요청에 대해서는
-//로컬 폴더 __dirname : main.js가 있는 폴더 위치
+// app.use('/pages', express.static(__dirname+'/pages')) //pages 경로로 들어오는 요청에 대해서는
+// 로컬 폴더 __dirname : main.js가 있는 폴더 위치
 // __dirname + '/pages'
 
-app.listen(3306, ()=>{
+app.listen(3000, ()=>{
     console.log("gg 누군가가 접속함.")
 })
 
 
-// 처리해주는 루틴 추가
-// app.get('/', (req,res)=>{
-//     console.log("루트에 요청이 들어옴")
-//     //res.send("루트에 대한 요청")
-//     res.sendFile(__dirname + '/pages/index.html')
-// })
+
+app.get('/', (req,res)=>{
+    console.log("루트에 요청이 들어옴")
+    //res.send("루트에 대한 요청")
+    res.sendFile(__dirname + '/pages/index.html')
+})
 
 // app.get('/about', (req,res)=>{
 //     console.log("/about에 요청이 들어옴")
@@ -24,9 +24,12 @@ app.listen(3306, ()=>{
 //     res.sendFile(__dirname + '/pages/about.html')
 // })
 
-// app.get('/working', (req,res)=>{
-//     console.log("/working에 요청이 들어옴")
-//     //res.send("/working에 대한 요청")
-//     res.sendFile(__dirname + '/pages/working.html')
-// })
+app.get('/working', (req,res)=>{
+    console.log("/working에 요청이 들어옴")
+    //res.send("/working에 대한 요청")
+    res.sendFile(__dirname + '/pages/working.html')
+})
 
+//pages 경로로 들어오는 요청에 대해서는 로컬폴더 __dirname : main.js가 있는 폴더 위치
+//__dirname + '/pages'
+app.use('/pages', express.static(__dirname+'/pages'))
